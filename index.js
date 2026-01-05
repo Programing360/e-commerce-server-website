33380ce0f48f4ab26c79769589ec8c5bdb4765d0
Md Limon <fhlimon360@gmail.com>
2026-01-06 00:26:37 +0600

    fixed the cart api and others option

--- index.js
+++ index.js
@@ -1,230 +1,273 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors')
-require('dotenv').config();
-const port = process.env.PORT || 5000
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
+app.use(cookieParser());
 app.use(express.json());
-app.use(cors())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3036qk8.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        const database = client.db("myProjectDB");
-        const usersCollection = database.collection("userProduct");
-        const cartCollection = database.collection("cart");
-        const ordersCollection = database.collection("orders");
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const database = client.db("myProjectDB");
+    const usersCollection = database.collection("userProduct");
+    const cartCollection = database.collection("cart");
+    const ordersCollection = database.collection("orders");
 
+    // Public
+    app.post("/jwt", async (req, res) => {
+      const { email } = req.body;
+       
+      if (!email) {
+        return res.status(400).send({ message: "Email required" });
+      }
 
-
-        app.get('/allProduct', async (req, res) => {
-            const result = await usersCollection.find().toArray();
-            res.send(result);
+      const user = { email }
+      const token = jwt.sign(user, process.env.JWT_ACCESS, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false, // true in production
+          sameSite: "lax",
+          maxAge: 24 * 60 * 60 * 1000,
         })
+        .send({ success: true });
+    });
 
-        app.get("/allProduct/:id", async (req, res) => {
-            try {
-                const id = req.params.id;
+    const verifyToken = (req, res, next) => {
+      const token = req.cookies?.token;
+      if (!token) {
+        console.log('unauthorized')
+        return res.status(401).send({ message: "Unauthorized access" });
+      }
 
-                const query = { _id: new ObjectId(id) }
+      jwt.verify(token, process.env.JWT_ACCESS, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "Unauthorized access" });
+        }
+        req.decoded = decoded;
+       
+        next();
+      });
+    };
 
-                const result = await usersCollection.findOne(query);
-                // console.log(result)
-                if (!result) {
-                    return res.status(404).send({ message: 'Product not found' });
-                }
+    app.get("/allProduct", verifyToken, async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
-                res.send(result);
-            } catch (error) {
-                res.status(500).send({ message: 'Invalid ID or server error' });
-            }
-        });
+    app.get("/allProduct/:id",  verifyToken, async (req, res) => {
+      try {
+        const id = req.params.id;
 
-        app.get('/cart', async (req, res) => {
-            const { email } = req.query;
+        const query = { _id: new ObjectId(id) };
 
-            if (!email) {
-                return res.status(400).send({ message: "Email is required" });
-            }
+        const result = await usersCollection.findOne(query);
+        // console.log(result)
+        if (!result) {
+          return res.status(404).send({ message: "Product not found" });
+        }
 
-            try {
-                const cartItems = await cartCollection
-                    .find({ "email": email })
-                    .sort({ createdAt: -1 })
-                    .toArray(); // convert cursor to array
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ message: "Invalid ID or server error" });
+      }
+    });
 
-                res.send(cartItems);
-                // const cartItems = await cartCollection.find().toArray();
-                // res.status(200).json(cartItems);
-            } catch (err) {
-                console.error(err);
-                res.status(500).json({ message: 'Server error' });
-            }
-        });
+    app.get("/cart", verifyToken, async (req, res) => {
+      const { email } = req.query;
 
+      if (!email) {
+        return res.status(400).send({ message: "Email is required" });
+      }
 
-        app.post('/allProduct', async (req, res) => {
-            const product = req.body
-            const result = await usersCollection.insertOne(product)
-            res.send(result)
-        })
+      try {
+        const cartItems = await cartCollection
+          .find({ email: email })
+          .sort({ createdAt: -1 })
+          .toArray(); // convert cursor to array
 
-        // place order
+        res.send(cartItems);
+        // const cartItems = await cartCollection.find().toArray();
+        // res.status(200).json(cartItems);
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Server error" });
+      }
+    });
 
-        app.get('/orderDetails', async (req, res) => {
-            const result = await ordersCollection.find().toArray()
-            res.send(result)
-        })
+    app.post("/allProduct",  verifyToken, async (req, res) => {
+      const product = req.body;
+      const result = await usersCollection.insertOne(product);
+      res.send(result);
+    });
 
-        // GET orders by customer email
-        app.get("/orders", async (req, res) => {
-            const { email } = req.query;
+    // place order
 
-            if (!email) {
-                return res.status(400).send({ message: "Email is required" });
-            }
+    app.get("/orderDetails", verifyToken, async (req, res) => {
+      const result = await ordersCollection.find().toArray();
+      res.send(result);
+    });
 
-            try {
-                const orders = await ordersCollection
-                    .find({ "customer.email": email })
-                    .sort({ createdAt: -1 })
-                    .toArray(); // convert cursor to array
+    // GET orders by customer email
+    app.get("/orders", verifyToken, async (req, res) => {
+      const { email } = req.query;
 
-                res.send(orders);
-            } catch (err) {
-                res.status(500).send({ message: "Server error", error: err.message });
-            }
+      if (!email) {
+        return res.status(400).send({ message: "Email is required" });
+      }
 
-            res.send(orders);
-        });
+      try {
+        const orders = await ordersCollection
+          .find({ "customer.email": email })
+          .sort({ createdAt: -1 })
+          .toArray(); // convert cursor to array
 
-        app.post('/orders', async (req, res) => {
-            const order = req.body;
-            const result = await ordersCollection.insertOne(order);
-            res.send(result);
-        });
+        res.send(orders);
+      } catch (err) {
+        res.status(500).send({ message: "Server error", error: err.message });
+      }
+    });
 
+    app.post("/orders",  verifyToken, async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order);
+      res.send(result);
+    });
 
+    // Add to cart
+    app.post("/cart/add",  verifyToken, async (req, res) => {
+      console.log(req.body)
+      try {
+        const { userId, productId, quantity, email, name, images, price } =
+          req.body;
+        const item = {
+          userId,
+          productId,
+          quantity,
+          email,
+          name,
+          images,
+          price,
+        };
+        // console.log(productId)
+        if (!item) {
+          return res.status(400).json({ message: "All fields are required" });
+        }
 
-        // Add to cart
-        app.post('/cart/add', async (req, res) => {
-            try {
-                const { userId, productId, quantity, email, name, images,price } = req.body;
-                const item = {
-                    userId, productId, quantity, email, name, images,price
-                }
-                // console.log(productId)
-                if (!item) {
-                    return res.status(400).json({ message: 'All fields are required' });
-                }
+        // Check if product already in cart
+        const existing = await cartCollection.findOne({ userId, productId });
 
-                // Check if product already in cart
-                const existing = await cartCollection.findOne({ userId, productId });
+        if (existing) {
+          // If exists, update quantity
+          await cartCollection.updateOne(
+            { _id: existing._id },
+            { $inc: { quantity: quantity } }
+          );
+        } else {
+          // Insert new item
+          const result = await cartCollection.insertOne(item);
+          // console.log(cartCollection)
+          res.send(result);
+        }
+      } catch (err) {
+        res.status(500).json({ message: "Server error" });
+      }
+    });
 
-                if (existing) {
-                    // If exists, update quantity
-                    await cartCollection.updateOne(
-                        { _id: existing._id },
-                        { $inc: { quantity: quantity } }
-                    );
-                } else {
-                    // Insert new item
-                    const result = await cartCollection.insertOne(item);
-                    // console.log(cartCollection)
-                    res.send(result)
-                }
+    app.put("/cart/update/:productId",  verifyToken, async (req, res) => {
+      const item = req.body;
+      // console.log(item)
+      const productId = req.params.productId;
+      const filter = { _id: new ObjectId(productId) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          quantity: item.quantity,
+        },
+      };
+      const cartItems = await cartCollection.findOne(filter);
 
+      if (!cartItems) {
+        return res.status(404).send({ message: "Item not found" });
+      }
 
-            } catch (err) {
-                res.status(500).json({ message: 'Server error' });
-            }
-        });
+      res.send(await cartCollection.updateOne(filter, updateDoc, options));
+    });
 
+    app.patch("/cart/increase/:id",  verifyToken,  async (req, res) => {
+      await cartCollection.updateOne(
+        { _id: new ObjectId(req.params.id) },
+        { $inc: { quantity: 1 } }
+      );
+      res.send({ success: true });
+    });
 
+    app.patch("/cart/decrease/:id",  verifyToken, async (req, res) => {
+      const cartItem = await cartCollection.findOne({
+        _id: new ObjectId(req.params.id),
+      });
+      if (cartItem.quantity > 1) {
+        await cartCollection.updateOne(
+          { _id: new ObjectId(req.params.id) },
+          { $inc: { quantity: -1 } }
+        );
+        res.send({ success: true });
+      } else {
+        res.status(400).send({ message: "Quantity cannot be less than 1" });
+      }
+    });
 
-        app.put('/cart/update/:productId', async (req, res) => {
-            const item = req.body;
-            console.log(item)
-            const productId = req.params.productId;
-            const filter = { _id: new ObjectId(productId) };
-            const options = { upsert: true };
-            const updateDoc = {
-                $set: {
-                    quantity: item.quantity
-                },
-            };
-            const cartItems = await cartCollection.findOne(filter);
+    // delete cart
+    app.delete("/cart/delete/:id",  verifyToken, async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
 
-            if (!cartItems) {
-                return res.status(404).send({ message: 'Item not found' });
-            }
-
-
-            res.send(await cartCollection.updateOne(filter, updateDoc, options));
-        })
-
-        app.patch('/cart/increase/:id', async (req, res) => {
-
-            await cartCollection.updateOne(
-                { _id: new ObjectId(req.params.id) },
-                { $inc: { quantity: 1 } }
-            );
-            res.send({ success: true });
-        });
-
-        app.patch('/cart/decrease/:id', async (req, res) => {
-            const cartItem = await cartCollection.findOne({ _id: new ObjectId(req.params.id) });
-            if (cartItem.quantity > 1) {
-                await cartCollection.updateOne(
-                    { _id: new ObjectId(req.params.id) },
-                    { $inc: { quantity: -1 } }
-                );
-                res.send({ success: true });
-            } else {
-                res.status(400).send({ message: 'Quantity cannot be less than 1' });
-            }
-        });
-
-        // delete cart
-        app.delete('/cart/delete/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await cartCollection.deleteOne(query);
-            res.send(result);
-
-        });
-
-
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-    res.send('Server is running 🚀');
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
